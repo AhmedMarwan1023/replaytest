4433c044458a58ce55a2f5382e3a1c4e4a22af64
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2018-11-29 17:41:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "M";
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -7,8 +8,27 @@ client.on('ready', () => {
 
 client.on('message', message => {
        if (message.content ==='هلا') { 
-            message.reply('اهلين وسهلين');
+            message.reply('*** اهلين وسهلين :rose: :heart_eyes: ***');
           }
 });
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+if (command == "embed") {
+    let say = new Discord.RichEmbed()
+  .setThumbnail(message.author.avatarURL)
+  .setAuthor(message.author.username)
+    .setDescription(args.join("  "))
+    .setColor(0x06DF00)
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+  });
 
 client.login(process.env.BOT_TOKEN);
