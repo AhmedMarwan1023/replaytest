2b135b622c7e5a049f2a2227ea705b6efe181524
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2018-11-29 17:30:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
@@ -8,11 +9,6 @@ client.on('message', message => {
        if (message.content ==='هلا') { 
             message.reply('اهلين وسهلين');
           }
-});
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', () => {
-    console.log('I am ready!');
 });
 
 client.login(process.env.BOT_TOKEN);
